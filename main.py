6f694091c51097f4a532b4d7ea0b4d4f1ade93b5
iamtonmoy0 <tonmoy2code@gmail.com>
2024-03-15 20:14:09 +0600

    Add new endpoints to get all items and get item by id.

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-
-
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# get items
+@app.get("/items")
+async def get_items():
+    return {"item": "this is items"}
+
+# get item by id
+@app.get("/items/{id}")
+async def get_item(id):
+    return {"item": id}
